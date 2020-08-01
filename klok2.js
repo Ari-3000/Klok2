d13c144de15f8461c9a44ff5041e20b1b7f022a1
Ari-3000 <68778698+Ari-3000@users.noreply.github.com>
2020-08-01 08:04:03 -0700

    workin

--- klok2.js
+++ klok2.js
@@ -1,7 +1,4 @@
 let s = 0; m = 0; h = 0;
-	var hVar = document.getElementById("hours");
-	var mVar = document.getElementById("minutes");
-	var sVar = document.getElementById("seconds");
 
 
 var minutes = 1000 * 60;
@@ -24,12 +21,39 @@ var y = g.getFullYear();    // year (YYYY)
 //document.write(k);
 
 
-var monthDays
+var monthDays = 0;
+if (k == 1) {
+  	monthDays = 31;
+} else if (k == 2) {
+  	monthDays = 31 + 29;
+  		// ^ gotta fix leap years etc.
+} else if (k == 3) {
+  	monthDays = 31 + 29 + 31;
+} else if (k == 4) {
+  	monthDays = 31 + 29 + 31 + 30;
+} else if (k == 5) {
+  	monthDays = 31 + 29 + 31 + 30 + 31;
+} else if (k == 6) {
+  	monthDays = 31 + 29 + 31 + 30 + 31 + 30;
+} else if (k == 7) {
+  	monthDays = 31 + 29 + 31 + 30 + 31 + 30 + 31;
+} else if (k == 8) {
+  	monthDays = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31;
+} else if (k == 9) {
+  	monthDays = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+} else if (k == 10) {
+  	monthDays = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+} else if (k == 11) {
+  	monthDays = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+} else if (k == 12) {
+  	monthDays = 31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31;
+}
 
-var t2 = t - ((y - 1970) * years) - 182*days  - (n * days);//(k * (30 * days))
+var t2 = t - ((y - 1970) * years) - monthDays  - (n * days);//(k * (30 * days))
 //^ gotta fix math & 30 d months
 document.write(t2);
           // Arnold Schwarzenegger / best movie EVAR (not the dumb blockbuster it seems) / milliseconds into today
+//h = 2;
 
 //var x = Date(y, k, n, 0, 0, 0);
 //document.write(x);
@@ -46,6 +70,9 @@ document.write(t2);
 
 			
 function seconds() {
+	var hVar = document.getElementById("hours");
+	var mVar = document.getElementById("minutes");
+	var sVar = document.getElementById("seconds");
 	if (s < 59) {
   		s++;
   		hVar.innerHTML = (h < 10 ? '0' : '') + h;
